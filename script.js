f419571527508b748e839e28e1c578605ce83281
MakersMarked <127469497+MakersMarked@users.noreply.github.com>
2023-04-16 13:05:34 -0700

    fix pixel size, adjust border colors

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ const redBtn = document.querySelector('.red');
 const blueBtn = document.querySelector('.blue')
 const greenBtn = document.querySelector('.green')
 const eraserBtn = document.querySelector('.eraser')
-let gridSize = 10
+const blackBtn = document.querySelector('.black')
+const yellowBtn = document.querySelector('.yellow')
+const rainbowBtn = document.querySelector('.rainbow')
+let gridSize = 8
 //Function to create a Div with relative dimensions
 function createGridDivs (size){
     const div = document.createElement('div');
         div.classList.add('squareDivs');   
         div.style.height =`${size}px`;
-        div.style.width =`${size}px`; 
-        
+        div.style.width =`${size}px`;   
     changeColor()
     return div  
 }
@@ -25,10 +27,10 @@ createGrid = () => {
         grid.appendChild(createGridDivs(grid.clientWidth / gridSize))
     }
 }
-createGrid()
 
+createGrid()
+//connects color buttons to drawing grid
 function changeColor() {
-    
     redBtn.addEventListener('click', () =>  grid.addEventListener('mouseover', (e) =>{
         e.target.style.backgroundColor = 'red'
             })
@@ -41,29 +43,32 @@ function changeColor() {
     }));
     eraserBtn.addEventListener('click', () => grid.addEventListener('mouseover', (e) =>{
         e.target.style.backgroundColor = 'white'
+    }));    
+    blackBtn.addEventListener('click', () => grid.addEventListener('mouseover', (e) =>{
+        e.target.style.backgroundColor = 'black'
     }));
-            
-        
+    yellowBtn.addEventListener('click', () => grid.addEventListener('mouseover', (e) =>{
+        e.target.style.backgroundColor = 'yellow'
+    }));
+            //figure out how to do rainbow
+    rainbowBtn.addEventListener('click', () => grid.addEventListener('mouseover', (e) =>{
+        e.target.style.backgroundColor = 'purple'
+    }));
 } 
-console.log(changeColor())
 function reset() {
     while (grid.firstChild) {
         grid.removeChild(grid.lastChild);
     }
     createGrid() 
 }
-
 input.addEventListener('input', (e) => {
     gridSize = e.target.value;
     inputText.textContent= `${gridSize} x ${gridSize}`;
 })
-
 apply.addEventListener('click', () =>{
-    
     reset()
     console.log(`${input.value}`)
 })
-
 resetBtn.addEventListener('click', () => {
     gridSize = 8
     reset()
